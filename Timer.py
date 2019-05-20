42799d4e2f13cf0b7a45cf918179bd823d06a81f
Ibrahim <ia903@nyu.edu>
2019-05-20 18:49:48 -0500

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,24 +1,3 @@
-#Part 1
-#Number 1:
-#Constructing a rectange using height, width, and character. 
-def Make_character_rectangle(height, width, char):
-    row = 1
-    while(row <= height):
-        print(width*char)
-        row+=1
-#Make_character_rectangle(2, 3, "^")
-
-#Number 2:
-#The parallelogram has to be slanted, so we need to consider the spaces that will be present in the beginning
-def Make_character_parallelogram(height,width,char):
-    row= 1
-    while(row <= height):
-        #height-row is multiplied by space because the number of space in the beginning is equal to height-row.
-       print((height-row)*' '+ (width*char))
-       row+=1
-#Make_character_parallelogram(8,19,'&')
-
-#Number 3:
 #Making a timer in format: Hours:Minutes:Seconds.fraction
 import time
 fraction=0
